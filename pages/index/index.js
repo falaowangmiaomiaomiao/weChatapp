2b06d6a0bf916893494a8d9841c466bcafcbfab0
falaowangmiaomiaomiao <1654178214@qq.com>
2018-09-12 18:46:55 +0800

    0.1.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,9 @@
 //index.js
+var wxCharts = require('../../utils/wxcharts.js');
 //获取应用实例
 const app = getApp();
+var lineChart = null;
+
 var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
 var qqmapsdk;
 Page({
@@ -8,12 +11,124 @@ Page({
     province: '',
     city: '',
     latitude: '',
-    longitude: ''
+    longitude: '',
+    //swiper位置配置
+    winWidth: 0,
+    winHeight: 0,
+    //页面切换
+    currentTab: 0,
   },
-  onLoad: function () {
+  //绘制图表相关-》
+  touchHandler: function (e) {
+    console.log(lineChart.getCurrentDataIndex(e));
+    lineChart.showToolTip(e, {
+      // background: '#7cb5ec',
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    for (var i = 0; i < 15; i++) {
+      categories.push(i + 1);
+      // data.push(Math.random() * (20 - 10) + 10);
+    }
+    // data[4] = null;
+    return {
+      categories: categories,
+      // data: data
+    }
+  },
+  // updateData: function () {
+  //   var simulationData = this.createSimulationData();
+  //   var series = [{
+  //     name: '成交量1',
+  //     data: simulationData.data,
+  //     format: function (val, name) {
+  //       return val.toFixed(2) + '万';
+  //     }
+  //   }];
+  //   lineChart.updateData({
+  //     categories: simulationData.categories,
+  //     series: series
+  //   });
+  // },//<-绘制图表相关
+  onLoad: function (options) {
     qqmapsdk = new QQMapWX({
       key: 'O2XBZ-M4EYG-C3CQZ-IP7PQ-B7NGS-YWFUU' //这里自己的key秘钥进行填充
     });
+    //tab页切换-》
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });//《-tab页切换
+
+
+    //->绘制图表相关
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      // background: '#f5f5f5',
+      series: [{
+        name: '降雨量',
+        data: [2, 0, 0, 3, 2, 4, 0, 0, 2, 0,3,2],
+        format: function (val, name) {
+          return val.toFixed(2) + 'mm';
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        // title: '成交金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: windowWidth,
+      height: 150,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });//《-绘制图表相关
+
+  },
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
   onShow: function () {
     let vm = this;
