6590376b5af58222f164157120a565058ff42e82
falaowangmiaomiaomiao <1654178214@qq.com>
2018-09-25 18:09:15 +0800

    0.1.9

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,15 +6,42 @@ var lineChart1 = null;
 var lineChart2 = null;
 var lineChart3 = null;
 var lineChart4 = null;
-var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
-var qqmapsdk;
+//->高德地图天气相关
+// var amapFile = require('../../utils/amap-wx.js');
+// var config = require('../../utils/config.js');
+//<-
+//->百度地图天气
+var bmap = require('../../utils/bmap-wx.min.js');
+//<-
+// var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
+// var qqmapsdk;
 Page({
   data: {
-    province: '',
-    city: '',
-    district:'',
-    latitude: '',
-    longitude: '',
+    //->高德天气相关
+    // city:"",
+    // province:"",
+    // weather:"",
+    // temperature:"",
+    //<-
+    // province: '',
+    // city: '',
+    // district:'',
+    // latitude: '',
+    // longitude: '',
+
+    //百度地图天气->
+    currentCity: '',
+    pm:"",
+    temperature:"",
+    weatherDesc:"",
+    date:"",
+    condition:"",
+    originalData:[],
+    urlIcon1: "",
+    urlIcon2: "",
+    url: "",
+    flag:"",
+    //<-
     //swiper位置配置
     winWidth: 0,
     winHeight: 0,
@@ -72,9 +99,156 @@ Page({
   },
   //<-绘制图表相关1,2,3,4
   onLoad: function (options) {
-    qqmapsdk = new QQMapWX({
-      key: 'O2XBZ-M4EYG-C3CQZ-IP7PQ-B7NGS-YWFUU' //这里自己的key秘钥进行填充
+    // qqmapsdk = new QQMapWX({
+    //   key: 'O2XBZ-M4EYG-C3CQZ-IP7PQ-B7NGS-YWFUU' //这里自己的key秘钥进行填充
+    // });
+    //->高德地图天气相关
+    // var that = this;
+    // var key = config.Config.key;
+    // var myAmapFun = new amapFile.AMapWX({ key: key });
+    // myAmapFun.getWeather({
+    //   success: function (data) {
+    //     if(data.city){
+    //       that.setData({
+    //         province: data.liveData.province,
+    //         city:data.liveData.city,
+    //         weather:data.weather.data,
+    //         temperature:data.temperature.data
+    //       });
+    //     }
+    //   },
+    //   fail: function (info) {
+    //     // wx.showModal({title:info.errMsg})
+    //   }
+    // })
+    //<-
+    //百度地图天气->
+    var that = this;
+    var BMap = new bmap.BMapWX({
+      ak: 'FQ0hekDSrd9c2zhO2HKeoOmjGAKj3KwW'
     });
+    var fail = function (data) {
+      console.log('fail!!!!')
+    };
+    var success = function (data) {
+      var flag=that.data.flag;
+      var weatherData = data.currentWeather[0];
+      var originalData = data.originalData.results[0].weather_data;
+      var urlIcon1,urlIcon2,url;
+      if (weatherData.weatherDesc.indexOf("暴雨")>=0){
+        url = "./image/baoyu.png"
+      } else if (weatherData.weatherDesc.indexOf("沙尘暴") >= 0) {
+        url = "./image/shachenbao.png"
+      } else if (weatherData.weatherDesc.indexOf("雾") >= 0) {
+        url = "./image/wu.png"
+      } else if (weatherData.weatherDesc.indexOf("阵雨") >= 0){
+        url = "./image/dyzzy.png"
+      } else if (weatherData.weatherDesc.indexOf("小雨") >= 0) {
+        url = "./image/xiaoyu.png"
+      } else if (weatherData.weatherDesc.indexOf("中雨") >= 0) {
+        url = "./image/xiaoyu.png"
+      } else if (weatherData.weatherDesc.indexOf("大雨") >= 0) {
+        url = "./image/xiaoyu.png"
+      } else if (weatherData.weatherDesc.indexOf("雨夹雪") >= 0) {
+        url = "./image/xiaoxue.png"
+      } else if (weatherData.weatherDesc.indexOf("小雪") >= 0) {
+        url = "./image/xiaoxue.png"
+      } else if (weatherData.weatherDesc.indexOf("中雪") >= 0) {
+        url = "./image/xiaoxue.png"
+      } else if (weatherData.weatherDesc.indexOf("大雪") >= 0) {
+        url = "./image/xiaoxue.png"
+      } else if (weatherData.weatherDesc.indexOf("多云") >= 0) {
+        url = "./image/qingtian.png"
+      } else if (weatherData.weatherDesc.indexOf("阴") >= 0) {
+        url = "./image/yin.png"
+      } else if (weatherData.weatherDesc.indexOf("晴") >= 0) {
+        url = "./image/qingtian2.png"
+      }
+
+      if (originalData[1].weather.indexOf("暴雨")>=0){
+        urlIcon1="./image/baoyuIcon.png"
+      } else if (originalData[1].weather.indexOf("阵雨") >= 0){
+        urlIcon1 = "./image/zhenyuIcon.png"
+      } else if (originalData[1].weather.indexOf("小雨") >= 0) {
+        urlIcon1 = "./image/xiaoyuIcon.png"
+      } else if (originalData[1].weather.indexOf("中雨") >= 0) {
+        urlIcon1 = "./image/xiaoyuIcon.png"
+      } else if (originalData[1].weather.indexOf("大雨") >= 0) {
+        urlIcon1 = "./image/xiaoyuIcon.png"
+      } else if (originalData[1].weather.indexOf("雨夹雪") >= 0) {
+        urlIcon1 = "./image/xiaoxueIcon.png"
+      } else if (originalData[1].weather.indexOf("小雪") >= 0) {
+        urlIcon1 = "./image/xiaoxueIcon.png"
+      } else if (originalData[1].weather.indexOf("中雪") >= 0) {
+        urlIcon1 = "./image/xiaoxueIcon.png"
+      } else if (originalData[1].weather.indexOf("大雪") >= 0) {
+        urlIcon1 = "./image/xiaoxueIcon.png"
+      } else if (originalData[1].weather.indexOf("多云") >= 0) {
+        urlIcon1 = "./image/duoyunIcon.png"
+      } else if (originalData[1].weather.indexOf("阴") >= 0) {
+        urlIcon1 = "./image/yinIcon.png"
+      } else if (originalData[1].weather.indexOf("晴") >= 0) {
+        urlIcon1 = "./image/qingIcon.png"
+      }
+      if (originalData[2].weather.indexOf("暴雨") >= 0) {
+        urlIcon2 = "./image/baoyuIcon.png"
+      } else if (originalData[2].weather.indexOf("阵雨") >= 0) {
+        urlIcon2 = "./image/zhenyuIcon.png"
+      } else if (originalData[2].weather.indexOf("小雨") >= 0) {
+        urlIcon2 = "./image/xiaoyuIcon.png"
+      } else if (originalData[2].weather.indexOf("中雨") >= 0) {
+        urlIcon2 = "./image/xiaoyuIcon.png"
+      } else if (originalData[2].weather.indexOf("大雨") >= 0) {
+        urlIcon2 = "./image/xiaoyuIcon.png"
+      } else if (originalData[2].weather.indexOf("雨夹雪") >= 0) {
+        urlIcon2 = "./image/xiaoxueIcon.png"
+      } else if (originalData[2].weather.indexOf("小雪") >= 0) {
+        urlIcon2 = "./image/xiaoxueIcon.png"
+      } else if (originalData[2].weather.indexOf("中雪") >= 0) {
+        urlIcon2 = "./image/xiaoxueIcon.png"
+      } else if (originalData[2].weather.indexOf("大雪") >= 0) {
+        urlIcon2 = "./image/xiaoxueIcon.png"
+      } else if (originalData[2].weather.indexOf("多云") >= 0) {
+        urlIcon2 = "./image/duoyunIcon.png"
+      } else if (originalData[2].weather.indexOf("阴") >= 0) {
+        urlIcon2 = "./image/yinIcon.png"
+      } else if (originalData[2].weather.indexOf("晴") >= 0) {
+        urlIcon2 = "./image/qingIcon.png"
+      }
+      var dates=weatherData.date.slice(-4,-2);
+      var pm25 = weatherData.pm25;
+      var condition
+      if(pm25<=50){
+        condition="优"
+      }else if(50<pm25<=100){
+        condition= "良"
+      } else if (100 < pm25 <= 150){
+        condition="轻度污染"
+      } else if (150 < pm25 <= 200) {
+        condition = "中度污染"
+      } else if (200 < pm25 <= 250) {
+        condition = "重度污染"
+      } else if (250 < pm25 <= 300) {
+        condition = "严重污染"
+      }
+      that.setData({
+        currentCity: weatherData.currentCity,
+        date: dates,
+        temperature: weatherData.temperature,
+        weatherDesc: weatherData.weatherDesc,
+        pm: pm25,
+        condition: condition,
+        originalData: originalData,
+        urlIcon1: urlIcon1,
+        urlIcon2: urlIcon2,
+        url:url
+      });
+    }
+    BMap.weather({
+      fail: fail,
+      success: success
+    });
+    //<-
     //tab页切换-》
     var that = this;
     wx.getSystemInfo({
@@ -256,109 +430,109 @@ Page({
       })
     }
   },
-  onShow: function () {
-    let vm = this;
-    vm.getUserLocation();
-  },
-  getUserLocation: function () {
-    let vm = this;
-    wx.getSetting({
-      success: (res) => {
-        console.log(JSON.stringify(res))
-        // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
-        // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
-        // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
-        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
-          wx.showModal({
-            title: '请求授权当前位置',
-            content: '需要获取您的地理位置，请确认授权',
-            success: function (res) {
-              if (res.cancel) {
-                wx.showToast({
-                  title: '拒绝授权',
-                  icon: 'none',
-                  duration: 1000
-                })
-              } else if (res.confirm) {
-                wx.openSetting({
-                  success: function (dataAu) {
-                    if (dataAu.authSetting["scope.userLocation"] == true) {
-                      wx.showToast({
-                        title: '授权成功',
-                        icon: 'success',
-                        duration: 1000
-                      })
-                      //再次授权，调用wx.getLocation的API
-                      vm.getLocation();
-                    } else {
-                      wx.showToast({
-                        title: '授权失败',
-                        icon: 'none',
-                        duration: 1000
-                      })
-                    }
-                  }
-                })
-              }
-            }
-          })
-        } else if (res.authSetting['scope.userLocation'] == undefined) {
-          //调用wx.getLocation的API
-          vm.getLocation();
-        }
-        else {
-          //调用wx.getLocation的API
-          vm.getLocation();
-        }
-      }
-    })
-  },
-  // 微信获得经纬度
-  getLocation: function () {
-    let vm = this;
-    wx.getLocation({
-      type: 'wgs84',
-      success: function (res) {
-        console.log(JSON.stringify(res))
-        var latitude = res.latitude
-        var longitude = res.longitude
-        var speed = res.speed
-        var accuracy = res.accuracy;
-        vm.getLocal(latitude, longitude)
-      },
-      fail: function (res) {
-        console.log('fail' + JSON.stringify(res))
-      }
-    })
-  },
-  // 获取当前地理位置
-  getLocal: function (latitude, longitude) {
-    let vm = this;
-    qqmapsdk.reverseGeocoder({
-      location: {
-        latitude: latitude,
-        longitude: longitude
-      },
-      success: function (res) {
-        console.log(JSON.stringify(res));
-        let province = res.result.ad_info.province
-        let city = res.result.ad_info.city
-        let district = res.result.ad_info.district
-        vm.setData({
-          province: province,
-          city: city,
-          district: district,
-          latitude: latitude,
-          longitude: longitude
-        })
+  // onShow: function () {
+  //   let vm = this;
+  //   vm.getUserLocation();
+  // },
+  // getUserLocation: function () {
+  //   let vm = this;
+  //   wx.getSetting({
+  //     success: (res) => {
+  //       console.log(JSON.stringify(res))
+  //       // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+  //       // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+  //       // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+  //       if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+  //         wx.showModal({
+  //           title: '请求授权当前位置',
+  //           content: '需要获取您的地理位置，请确认授权',
+  //           success: function (res) {
+  //             if (res.cancel) {
+  //               wx.showToast({
+  //                 title: '拒绝授权',
+  //                 icon: 'none',
+  //                 duration: 1000
+  //               })
+  //             } else if (res.confirm) {
+  //               wx.openSetting({
+  //                 success: function (dataAu) {
+  //                   if (dataAu.authSetting["scope.userLocation"] == true) {
+  //                     wx.showToast({
+  //                       title: '授权成功',
+  //                       icon: 'success',
+  //                       duration: 1000
+  //                     })
+  //                     //再次授权，调用wx.getLocation的API
+  //                     vm.getLocation();
+  //                   } else {
+  //                     wx.showToast({
+  //                       title: '授权失败',
+  //                       icon: 'none',
+  //                       duration: 1000
+  //                     })
+  //                   }
+  //                 }
+  //               })
+  //             }
+  //           }
+  //         })
+  //       } else if (res.authSetting['scope.userLocation'] == undefined) {
+  //         //调用wx.getLocation的API
+  //         vm.getLocation();
+  //       }
+  //       else {
+  //         //调用wx.getLocation的API
+  //         vm.getLocation();
+  //       }
+  //     }
+  //   })
+  // },
+  // // 微信获得经纬度
+  // getLocation: function () {
+  //   let vm = this;
+  //   wx.getLocation({
+  //     type: 'wgs84',
+  //     success: function (res) {
+  //       console.log(JSON.stringify(res))
+  //       var latitude = res.latitude
+  //       var longitude = res.longitude
+  //       var speed = res.speed
+  //       var accuracy = res.accuracy;
+  //       vm.getLocal(latitude, longitude)
+  //     },
+  //     fail: function (res) {
+  //       console.log('fail' + JSON.stringify(res))
+  //     }
+  //   })
+  // },
+  // // 获取当前地理位置
+  // getLocal: function (latitude, longitude) {
+  //   let vm = this;
+  //   qqmapsdk.reverseGeocoder({
+  //     location: {
+  //       latitude: latitude,
+  //       longitude: longitude
+  //     },
+  //     success: function (res) {
+  //       console.log(JSON.stringify(res));
+  //       let province = res.result.ad_info.province
+  //       let city = res.result.ad_info.city
+  //       let district = res.result.ad_info.district
+  //       vm.setData({
+  //         province: province,
+  //         city: city,
+  //         district: district,
+  //         latitude: latitude,
+  //         longitude: longitude
+  //       })
 
-      },
-      fail: function (res) {
-        console.log(res);
-      },
-      complete: function (res) {
-        // console.log(res);
-      }
-    });
-  }
+  //     },
+  //     fail: function (res) {
+  //       console.log(res);
+  //     },
+  //     complete: function (res) {
+  //       // console.log(res);
+  //     }
+  //   });
+  // }
 })
---
+++ utils/config.js
@@ -0,0 +1,5 @@
+var config = {
+  key: '5e3e9a92ef000fb8e1aa62214192aba9'
+}
+
+module.exports.Config = config;
