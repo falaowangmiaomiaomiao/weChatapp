827512e96343dff1aca31b364097cb70b71f6073
falaowangmiaomiaomiao <1654178214@qq.com>
2018-09-29 18:03:34 +0800

    0.2.1

--- pages/waterValve/waterValve.js
+++ pages/waterValve/waterValve.js
@@ -1,4 +1,5 @@
 // pages/waterValve/waterValve.js
+const app = getApp();
 Page({
 
   /**
@@ -14,33 +15,45 @@ Page({
     //   { id: 5, item: [{ nameA: "张三" }, { nameB: "李四" }, { num: 1234567897656 }, { error: false }], checked: false,  key: 5 },
     // ]
     list:[],
+    isFormSearch: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that=this;
-    var list=that.data.list;
-    wx.request({
-      url: 'https://weixin.yaoshihe.cn:950/peasant/operation/allValveList',
-      data:{
-        
-      },
-      header: {
-        'content-type': 'application/json'
-      },
-      method:"POST",
-      success(res){
-        // console.log(res.data.data);
-        var data=res.data.data
-        that.setData({
-          list:data
-        })
+    //->跳转页面实现搜索
+    if (options && options.searchValue) {
+      var value = options.searchValue;
+      console.log(value)
+      if (value.length == 0) {
+        return;
       }
+      this.isFormSearch = true;
+      wx.request({
+        url: 'https://weixin.yaoshihe.cn:950/peasant/operation/allValveList?name='+value,
+        data: {
+
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        method: "POST",
+        success: (res) => {
+          this.setData({
+            list: res.data.data
+          });
+        },
+      })
+    }
+    //<-
+
+  },
+  wxSearchTab:function(){
+    wx.redirectTo({
+      url: '../search/search'
     })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -51,7 +64,30 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function (options) {
+    if (this.isFormSearch) {
+      this.isFormSearch = false;
+      return;
+    }
+    let that = this;
+    var list = that.data.list;
+    wx.request({
+      url: 'https://weixin.yaoshihe.cn:950/peasant/operation/allValveList',
+      data: {
+
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "POST",
+      success(res) {
+        // console.log(res.data.data);
+        var data = res.data.data
+        that.setData({
+          list: data
+        })
+      }
+    })
 
   },
 
